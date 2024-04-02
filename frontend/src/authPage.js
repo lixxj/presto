7e6c532f4ea33a68d5d7bb8b09a2964d6b76b0ff
z5420776 <z5420776@ad.unsw.edu.au>
2024-04-02 23:26:00 +1100

    login and register forms

--- frontend/src/authPage.js
+++ frontend/src/authPage.js
@@ -1,32 +1,45 @@
 import React, { useState } from 'react';
+import styles from './authPage.module.css';
 
 function authForms () {
   const [loginState, toggleState] = useState(true);
 
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [confirmPassword, setConfirmPassword] = React.useState('')
+
   const switchForms = (event) => {
     event.preventDefault()
     toggleState(!loginState);
-    console.log(loginState)
   }
 
   return (
-    <form>
+    <div>
       {loginState
         ? (
-        <div>
+        <form>
           <h1>Presto</h1>
           <h2>Login</h2>
+          <input placeholder='Email' onChange={e => setEmail(e.target.value)}></input>
+          <input placeholder='Password' type='password' onChange={e => setPassword(e.target.value)}></input>
+          <button onClick={test}>Login</button>
           <button onClick={switchForms}>Register</button>
-        </div>
+        </form>
           )
         : (
-        <div>
+        <form>
           <h1>Presto</h1>
           <h2>Register</h2>
+          <input placeholder='Name' onChange={e => setName(e.target.value)}></input>
+          <input placeholder='Email' onChange={e => setEmail(e.target.value)}></input>
+          <input placeholder='Password' type='password' onChange={e => setPassword(e.target.value)}></input>
+          <input placeholder='Confirm Password' type='password' onChange={e => setConfirmPassword(e.target.value)}></input>
+          <button onClick={test}>Register</button>
           <button onClick={switchForms}>Back to Login</button>
-        </div>
+        </form>
           )}
-    </form>
+    </div>
   );
 }
 
