cb94c479e5e4692e7657ec69fc241074b820f9ea
XJ Li <z5190719@ad.unsw.edu.au>
2024-04-08 11:06:42 +1000

    change new pre structure modal->newpreButton->dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,35 +1,41 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import axios from 'axios';
+import NewPresentationButton from '../components/NewPresentationButton';
 
-function Dashboard ({ token, setTokenFunction }) {
+function Dashboard ({ token, setTokenFunction, darkMode, addPresentation }) {
   const [store, setStore] = React.useState({});
+  console.log(store);
 
   React.useEffect(() => {
     if (token) {
       axios.get('http://localhost:5005/store', {
         headers: {
           Authorization: token,
-        }
+        },
       }).then((response) => {
         setStore(response.data.store);
-      }).catch(error => {
+      }).catch((error) => {
         console.error('Error fetching data: ', error);
-        // Handle error appropriately in your UI
       });
     }
   }, [token]);
 
-  console.log(store);
-
   if (token === null) {
     return <Navigate to="/login" />;
   }
+
   return (
-    <>
-      {/* dashboard content... */}
-      dashboard content...
-    </>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <div style={{ flexGrow: 1 }}>
+        {/* Reserve this area for presentation content */}
+
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '10px', marginRight: '-20px', }}>
+        {/* NewPresentationButton always aligned to the right */}
+        {token && <NewPresentationButton darkMode={darkMode} addPresentation={addPresentation} />}
+      </div>
+    </div>
   );
 }
 
